9e5782d7c9c109bd2e05a3b83c522e1cdb7e8e20
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    [neobuddy89]
    Clean up unused code and convert to 1-100 scale.
    
    Change-Id: If1359191bbf1628143bde028bc947e3765f513fe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7209,6 +7209,11 @@ public final class Settings {
         public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
