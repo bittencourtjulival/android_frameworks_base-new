5ef3e88b7b8af4e7d4536f0557b48d0b33277359
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 19:07:49 +0800

    EdgeEffect: Implement intensity-based edge scroll fling haptics
    squashed:
    
    	From: minaripenguin <minaripenguin@users.noreply.github.com>
    	Date: Sat, 7 Dec 2024 17:13:14 +0800
    	Subject: [PATCH] EdgeEffect: Add rate limit to vibration haptic effect
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7203,6 +7203,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_HEIGHT = "status_bar_custom_header_height";
 
         /**
+         * @hide
+         */
+        @Readable
+        public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
