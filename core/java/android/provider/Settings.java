c9636bfda9ce6e26830bc53c0d9f0377dcbd4f39
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 18:30:27 +0800

    SystemUI: Implement intensity-based QS tiles haptic feedback
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7376,6 +7376,11 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_TILE_HAPTIC = "qs_panel_tile_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
