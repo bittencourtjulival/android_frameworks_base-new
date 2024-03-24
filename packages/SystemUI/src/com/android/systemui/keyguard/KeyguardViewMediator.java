dd9b0199d4b309ef8ea530ccf607efc2971df044
timjosten <timjosten@gmail.com>
2024-03-24 22:27:20 +0800

    SystemUI: Fix double unlock sound issue
    Change-Id: Ie3716656807e6b8ac531efe8cbe618612db6afba
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -513,6 +513,7 @@ public class KeyguardViewMediator implements CoreStartable,
     private boolean mHideAnimationRunning = false;
     private boolean mIsKeyguardExitAnimationCanceled = false;
 
+    private long mLastTimeSoundWasPlayed = 0;
     private SoundPool mLockSounds;
     private int mLockSoundId;
     private int mUnlockSoundId;
@@ -2971,6 +2972,8 @@ public class KeyguardViewMediator implements CoreStartable,
         int lockscreenSoundsEnabled = mSystemSettings.getIntForUser(LOCKSCREEN_SOUNDS_ENABLED, 1,
                 mSelectedUserInteractor.getSelectedUserId());
         if (lockscreenSoundsEnabled == 1) {
+            if (mSystemClock.elapsedRealtime() - mLastTimeSoundWasPlayed < 300) return;
+            mLastTimeSoundWasPlayed = mSystemClock.elapsedRealtime();
 
             mLockSounds.stop(mLockSoundStreamId);
             // Init mAudioManager
