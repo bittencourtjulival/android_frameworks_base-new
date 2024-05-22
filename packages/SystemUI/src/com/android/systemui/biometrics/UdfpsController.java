19c31f817dc3bde755d671e3cecf7287a02e542d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-22 08:24:18 +0800

    SystemUI: Fix Udfps animation positioning logic
    * current implementation doesnt seem to work well on high resolution devices like cheetah 1080x3120 (full resolution).
      Usually migrating to AuthController for the positioning logic fixes the issue however for some reason when we switch
      back to high resolution (1080x2340) the udfps animation positioning is broken, to fix the inconsistencies and the positioning logic,
      use AuthController for full resolution and fallback to fingerprint props when device is using high resolution.
    * properly re-initialized the udfps animation on resolution change (onThemeChanged) and other possible configuration change
    * turn off debugging since most issues have been fixed
    
    test: Switch from high-full resolution on cheetah and vice versa. UDPFS animation location is accurate
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -36,6 +36,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
@@ -81,6 +82,7 @@ import com.android.keyguard.UserActivityNotifier;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Flags;
 import com.android.systemui.animation.ActivityTransitionAnimator;
+import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.biometrics.dagger.BiometricsBackground;
 import com.android.systemui.biometrics.domain.interactor.UdfpsOverlayInteractor;
 import com.android.systemui.biometrics.shared.model.UdfpsOverlayParams;
@@ -192,6 +194,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     @NonNull private final PowerInteractor mPowerInteractor;
     @NonNull private final CoroutineScope mScope;
     @NonNull private final InputManager mInputManager;
+    @NonNull private final AuthController mAuthController;
     @NonNull private final SelectedUserInteractor mSelectedUserInteractor;
     private final boolean mIgnoreRefreshRate;
     private final KeyguardTransitionInteractor mKeyguardTransitionInteractor;
@@ -271,6 +274,24 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mScreenOn = false;
         }
     };
+
+    private ConfigurationController.ConfigurationListener mConfigurationListener =
+            new ConfigurationController.ConfigurationListener() {
+                @Override
+                public void onThemeChanged() {
+                    updateUdfpsAnimation();
+                }
+
+                @Override
+                public void onUiModeChanged() {
+                    updateUdfpsAnimation();
+                }
+
+                @Override
+                public void onConfigChanged(Configuration newConfig) {
+                    updateUdfpsAnimation();
+                }
+            };
 
     @Override
     public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
@@ -732,7 +753,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             @NonNull UdfpsOverlayInteractor udfpsOverlayInteractor,
             @NonNull PowerInteractor powerInteractor,
             @Application CoroutineScope scope,
-            UserActivityNotifier userActivityNotifier) {
+            UserActivityNotifier userActivityNotifier,
+            @NonNull AuthController authController) {
         mContext = context;
         mExecution = execution;
         mVibrator = vibrator;
@@ -780,6 +802,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mDefaultUdfpsTouchOverlayViewModel = defaultUdfpsTouchOverlayViewModel;
 
         mDumpManager.registerDumpable(TAG, this);
+        
+        mAuthController = authController;
 
         mOrientationListener = new BiometricDisplayListener(
                 context,
@@ -828,9 +852,21 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             );
         }
 
+        updateUdfpsAnimation();
+        mConfigurationController.addCallback(mConfigurationListener);
+    }
+
+    private void updateUdfpsAnimation() {
         if (com.android.internal.util.android.Utils.isPackageInstalled(mContext,
                 "com.crdroid.udfps.animations")) {
-            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+            if (mUdfpsAnimation != null) {
+                mUdfpsAnimation.removeAnimation();
+                mUdfpsAnimation = null;
+            }
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
+            if (mUdfpsAnimation != null) {
+                mUdfpsAnimation.updatePosition();
+            }
         }
     }
 
