28dca8332290981636684096a9cec062ed9e8ae8
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-11 17:03:17 +0800

    services: Introduce Smart Power Off feature
    * an automated power off service that delays itself when there is a user input, requires an active alarm (for the purpose of waking up the device)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -120,6 +120,8 @@ import static com.android.systemui.shared.Flags.enableLppSqueezeEffect;
 
 import static org.lineageos.internal.util.DeviceKeysConstants.*;
 
+import org.rising.server.*;
+
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.Nullable;
 import android.annotation.SuppressLint;
@@ -914,6 +916,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mLongSwipeDown;
     private CameraAvailbilityListener mCameraAvailabilityListener;
 
+    private SmartPowerOffService mSmartPowerOffService;
+    private boolean mSmartPowerOffEnabled;
+
     private class PolicyHandler extends Handler {
 
         private PolicyHandler(Looper looper) {
@@ -1191,6 +1196,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.KEY_SHAKE_GESTURE_ACTION), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    "smart_power_off_enabled"), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -3668,6 +3676,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mVolUpAndDownMute = LineageSettings.System.getIntForUser(resolver,
                     LineageSettings.System.VOLUME_UP_AND_DOWN_MUTE, 0,
                     UserHandle.USER_CURRENT) == 1;
+
+            boolean smartPowerOffEnabled = Settings.System.getIntForUser(resolver,
+                    "smart_power_off_enabled", 0,
+                    UserHandle.USER_CURRENT) != 0;
+            if (mSmartPowerOffService != null && mSmartPowerOffEnabled != smartPowerOffEnabled) {
+                mSmartPowerOffEnabled = smartPowerOffEnabled;
+                if (mSmartPowerOffEnabled) {
+                    mWindowManagerFuncs.registerPointerEventListener(mSmartPowerOffService, DEFAULT_DISPLAY);
+                } else {
+                    mWindowManagerFuncs.unregisterPointerEventListener(mSmartPowerOffService, DEFAULT_DISPLAY);
+                }
+            }
 
             // Configure wake gesture.
             boolean wakeGestureEnabledSetting = Settings.Secure.getIntForUser(resolver,
@@ -7753,6 +7773,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
+
+        mSmartPowerOffService = new SmartPowerOffService(mContext);
+        mSmartPowerOffService.start();
     }
 
     /** {@inheritDoc} */
