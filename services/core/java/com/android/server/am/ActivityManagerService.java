ebeace91197d116f7139535562504a452389385e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2024-11-24 19:58:36 +0800

    services: Extend long screen feature to all apps
    original commit: https://github.com/RisingTechOSS/android_frameworks_base/commit/6208b06c21a4483d13d17b6d238225d1b51cf928:
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I089434b2849226c9ea17acd4d43444714bef4c57

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19546,4 +19546,9 @@ public class ActivityManagerService extends IActivityManager.Stub
             return mIsSwipeToScrenshotEnabled && SystemProperties.getBoolean("sys.android.screenshot", false);
         }
     }
+    
+    @Override
+    public boolean shouldForceLongScreen(String packageName) {
+        return mActivityTaskManager.shouldForceLongScreen(packageName);
+    }
 }
