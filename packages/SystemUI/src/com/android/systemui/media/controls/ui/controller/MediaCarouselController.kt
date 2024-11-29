e91205484fbcdd634053fec23be4a4fd139351b3
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-29 17:05:16 +0800

    SystemUI: Introduce Notification Peek Display feature [1/2]
    * inspired from motorola lockscreen bubble style and android 15/16 beta minimalism feature
    
    Arman-ATI: Adapt to A16
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Arman Altafi <Arman-ATI@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaCarouselController.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaCarouselController.kt
@@ -405,6 +405,7 @@ constructor(
             }
         }
         listenForLockscreenSettingChanges(applicationScope)
+        listenForPeekDisplayExpansionChanges(applicationScope)
 
         // Notifies all active players about animation scale changes.
         bgExecutor.execute {
@@ -567,6 +568,21 @@ constructor(
                 }
         }
     }
+    
+    @VisibleForTesting
+    internal fun listenForPeekDisplayExpansionChanges(scope: CoroutineScope): Job {
+        return scope.launch {
+            systemSettings
+                .observerFlow(UserHandle.USER_ALL, "peek_display_expanded")
+                .onStart { emit(Unit) }
+                .map { getMediaLockScreenSetting() }
+                .distinctUntilChanged()
+                .collectLatest {
+                    allowMediaPlayerOnLockScreen = it
+                    updateHostVisibility()
+                }
+        }
+    }
 
     @VisibleForTesting
     internal fun listenForAnyStateToDozingTransition(scope: CoroutineScope): Job {
@@ -698,6 +714,12 @@ constructor(
                 true,
                 UserHandle.USER_CURRENT,
             )
+            val isPeekDisplayExpanded = systemSettings.getBoolForUser(
+                "peek_display_expanded",
+                false,
+                UserHandle.USER_CURRENT
+            )
+            isMediaControlsEnabled && !isPeekDisplayExpanded
         }
     }
 
