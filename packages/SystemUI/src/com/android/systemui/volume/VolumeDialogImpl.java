09ed9ff08cdddb823beb99907d29e98586ce5030
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-03 10:23:37 +0800

    SystemUI: Implement intensity-based volume slider haptics
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1176,8 +1176,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     private void addSliderHapticsToRow(VolumeRow row) {
-        row.createPlugin(row.slider, mVibratorHelper, mMSDLPlayer, mSystemClock);
-        HapticSliderViewBinder.bind(row.slider, row.mHapticPlugin);
     }
 
     @VisibleForTesting void addSliderHapticsToRows() {
@@ -2713,6 +2711,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             return;  // don't update if user is sliding
         }
         final int progress = row.slider.getProgress();
+        mVolumeUtils.performVolumeHaptics(mShowing, progress, row.ss.levelMax);
         final int level = getVolumeFromProgress(row.ss, row.slider, progress);
         final boolean rowVisible = row.view.getVisibility() == VISIBLE;
         final boolean inGracePeriod = (SystemClock.uptimeMillis() - row.userAttempt)
@@ -3261,6 +3260,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            mVolumeUtils.performVolumeHaptics(mShowing, mRow.slider.getProgress(), mRow.ss.levelMax);
         }
 
         @Override
