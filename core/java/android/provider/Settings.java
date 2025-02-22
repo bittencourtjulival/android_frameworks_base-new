c59dd0b68fca5542eebde35f500feccf8df5e494
NurKeinNeid <nurkeinneid@derpfest.org>
2025-02-22 16:14:01 +0100

    SystemUI: Add ongoing action chip toggle setting [1/2]
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7318,6 +7318,11 @@ public final class Settings {
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
         /**
+         * @hide
+         */
+        public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
