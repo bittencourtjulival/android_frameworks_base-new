49c00a00541bd73f64d1dab58ae4d1c4c12b8f4e
Ghosuto <clash.raja10@gmail.com>
2025-03-21 16:25:28 +0000

    SystemUI: Introduce media playback progress bar in action chip
    - Add support for displaying media playback progress in the status bar.
    - Integrate MediaSessionManagerHelper (thanks to RisingOS and AxionAOSP) to track media playback state.
    - Show media progress only when music/video is playing; hide during downloads.
    - Add gesture controls:
      - Single tap: stop media.
      - Double tap: Skip to the next track.
      - Long press: Open the media app.
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7326,6 +7326,11 @@ public final class Settings {
         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
 
         /**
+         * @hide
+         */
+         public static final String SHOW_MEDIA_PROGRESS = "show_media_progress";
+
+        /**
          * Whether to enable Smart Pixels
          * @hide
          */
