b76c74b1387addf0e2a0df17047562167921aaec
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-26 18:11:15 +0800

    SystemUI: Fix face unlock indication static calls
    Change-Id: Id216d9b6675e05d929d1ef61e5d76a8f43a4c27e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -118,6 +118,7 @@ import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.res.R;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.phone.FaceUnlockImageView;
+import com.android.systemui.statusbar.phone.FaceUnlockProxy;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.phone.KeyguardIndicationTextView;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
@@ -1107,9 +1108,6 @@ public class KeyguardIndicationController {
     }
 
     private void hideFaceUnlockRecognizingMessage() {
-        if (mFaceIconView != null) {
-            mFaceIconView.setVisibility(View.GONE);
-        }
         String faceUnlockMessage = mContext.getResources().getString(
             R.string.face_unlock_recognizing);
         if (mBiometricMessage != null && mBiometricMessage.equals(faceUnlockMessage)) {
@@ -1900,9 +1898,7 @@ public class KeyguardIndicationController {
     };
 
     private void updateFaceIconViewState(FaceUnlockImageView.State state) {
-        if (mFaceIconView != null) {
-            mFaceIconView.setState(state);
-        }
+        FaceUnlockProxy.INSTANCE().setBouncerState(state);
     }
 
     private final KeyguardStateController.Callback mKeyguardStateCallback =
