91499bba4701b82125f4768d73ede03fd8b8e19d
lijilou <lijilou@xiaomi.corp-partner.google.com>
2025-04-22 19:33:43 +0800

    Remove unnecessary global lock and make package private for removeObsoleteTaskFiles method.
    The removeObsoleteTaskFiles method of WMS is only called in onPreProcessItem method of TaskPersister file and have added global lock,so we can remove the global lock and make this method package private.
    
    Bug: none
    Flag: EXEMPT minor optimization
    Google: 3599311
    Change-Id: Ie73698004f79104cd012aae5cb61e46926717cdf

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -4354,10 +4354,8 @@ public class WindowManagerService extends IWindowManager.Stub
      * @param runningUserIds The ids of the list of users that have tasks loaded in our in-memory
      *                       model.
      */
-    public void removeObsoleteTaskFiles(ArraySet<Integer> persistentTaskIds, int[] runningUserIds) {
-        synchronized (mGlobalLock) {
-            mTaskSnapshotController.removeObsoleteTaskFiles(persistentTaskIds, runningUserIds);
-        }
+    void removeObsoleteTaskFiles(ArraySet<Integer> persistentTaskIds, int[] runningUserIds) {
+        mTaskSnapshotController.removeObsoleteTaskFiles(persistentTaskIds, runningUserIds);
     }
 
     @Override
