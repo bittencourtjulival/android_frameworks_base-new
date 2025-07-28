crdroidandroid/android_frameworks_base

SystemUI: Fix crash when resetting ShadeController views
java.lang.NullPointerException: Attempt to invoke interface method 'void com.android.systemui.shade.ShadeViewController.resetViews(boolean)' on a null object reference
	at com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager.reset(StatusBarKeyguardViewManager.java:860)
	at com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager.show(StatusBarKeyguardViewManager.java:662)
	at com.android.systemui.keyguard.KeyguardViewMediator.handleShow(KeyguardViewMediator.java:2780)
	at com.android.systemui.keyguard.KeyguardViewMediator.-$$Nest$mhandleShow(KeyguardViewMediator.java:0)
	at com.android.systemui.keyguard.KeyguardViewMediator$12.handleMessage(KeyguardViewMediator.java:2498)
	at android.os.Handler.dispatchMessage(Handler.java:107)
	at android.os.Looper.loopOnce(Looper.java:232)
	at android.os.Looper.loop(Looper.java:317)
	at android.app.ActivityThread.main(ActivityThread.java:8527)
	at java.lang.reflect.Method.invoke(Native Method)
	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:555)
	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878)

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -817,7 +817,9 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         if (mKeyguardStateController.isShowing() && !bouncerIsAnimatingAway()) {
             final boolean isOccluded = mKeyguardStateController.isOccluded();
             // Hide quick settings.
-            mShadeViewController.resetViews(/* animate= */ !isOccluded);
+            if (mShadeViewController != null) {
+                mShadeViewController.resetViews(/* animate= */ !isOccluded);
+            }
             // Hide bouncer and quick-quick settings.
             if (isOccluded && !mDozing) {
                 mCentralSurfaces.hideKeyguard();

bittencourtjulival/android_frameworks_base-new

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -986,7 +986,9 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         if (mKeyguardStateController.isShowing() && !bouncerIsAnimatingAway()) {
             final boolean isOccluded = mKeyguardStateController.isOccluded();
             // Hide quick settings.
-            mShadeLockscreenInteractor.resetViews(/* animate= */ !isOccluded);
+            if (mShadeLockscreenInteractor != null) {
+                mShadeLockscreenInteractor.resetViews(/* animate= */ !isOccluded);
+            }
             // Hide bouncer and quick-quick settings.
             if (isOccluded && !mDozing) {
                 mCentralSurfaces.hideKeyguard();